0fd23bef779d1e1091ca7e87f8d5b42b7046fd8b
JordanC-byte <jcummings2@albany.edu>
2025-03-24 19:38:16 -0400

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,8 @@
+// Using let
+let name = "Crystal";
+let age = "unknown";
+let isStudent = true
+
+document.getElementById("demo1").innerHTML = 
+  "Name: " + name + "<br>Age: " + age + "<br>Student:" + isStudent;
+
