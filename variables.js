aed8b6862f55cff341033d1e66937037e9fc1e0f
JordanC-byte <jcummings2@albany.edu>
2025-03-24 20:01:13 -0400

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,16 +1,16 @@
 // Using let
-let name = "Jordan";
-let age = 19;
-let isStudent = true;
+//let name = "Jordan";
+//let age = 19;
+//let isStudent = true;
 
-document.getElementById("demo1").innerHTML = 
-  "Name: " + name + "<br>Age: " + age + "<br>Student:" + isStudent;
+//document.getElementById("demo1").innerHTML = 
+//  "Name: " + name + "<br>Age: " + age + "<br>Student: " + isStudent;
 
-//var city = "Albany";   // Function-scoped
-//let state = "NY";      // Block-scoped
-//const country = "USA"; // Cannot be reassigned
-//var favcolor = "green";
+var city = "Albany";   // Function-scoped
+let state = "NY";      // Block-scoped
+const country = "USA"; // Cannot be reassigned
+var favcolor = "green";
 
 
-//document. getElementById ("demo2").innerHTML =
-// "Location: " + city +", " + state + ", " + country;
+document. getElementById ("demo2").innerHTML =
+ "Location: " + city +", " + state + ", " + country;
